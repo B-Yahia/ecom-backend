31de074c53425b22a95cd2744ee284352f0cce65
yahya <yahiabahhous@gmail.com>
2024-08-21 12:30:54 +0300

    cros config

--- src/config/Setup.php
+++ src/config/Setup.php
@@ -13,30 +13,17 @@ class Setup
 
     public static function cros(string $url): void
     {
-        if (isset($_SERVER['HTTP_ORIGIN'])) {
-            header("Access-Control-Allow-Origin: " . $url);
-            header('Access-Control-Allow-Credentials: true');
-            header('Access-Control-Max-Age: 86400');
+
+        header("Access-Control-Allow-Origin: " . $url);
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');
+        header("Access-Control-Allow-Methods: GET, POST");
+        header("Access-Control-Allow-Headers: Content-Type, Authorization");
+        if (!in_array($_SERVER['REQUEST_METHOD'], self::$allowed_methods)) {
+            header('HTTP/1.1 405 Method Not Allowed');
+            header('Allow: GET, POST');
+            exit;
         }
-        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
-                header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-            }
-            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
-                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-            }
-            exit(0);
-        }
-        // header("Access-Control-Allow-Origin: " . $url);
-        // header('Access-Control-Allow-Credentials: true');
-        // header('Access-Control-Max-Age: 86400');
-        // header("Access-Control-Allow-Methods: GET, POST");
-        // header("Access-Control-Allow-Headers: Content-Type, Authorization");
-        // if (!in_array($_SERVER['REQUEST_METHOD'], self::$allowed_methods)) {
-        //     header('HTTP/1.1 405 Method Not Allowed');
-        //     header('Allow: GET, POST');
-        //     exit;
-        // }
     }
 
     public static function database(): Database
